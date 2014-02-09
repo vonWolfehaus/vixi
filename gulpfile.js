df44cccf4d13da139bc144dc772bc65ac23c3154
Corey Birnbaum <vonwolfehaus@gmail.com>
2014-02-08 20:33:13 -0800

    added dist and build system

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var rjs = require('gulp-requirejs');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('build-geom', function() {
+	gulp.src('src/geom/*.js')
+		.pipe(concat('lib-geom.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', function() {
+	rjs({
+		baseUrl: 'src',
+		out: 'von-canvas2d.js',
+		include: ['../lib/LinkedList.js', 'Container', 'Sprite'],
+		wrap: true,
+		keepAmdefine: false
+	})
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('all', ['build-geom', 'build'], function() {
+	gulp.src(['dist/lib-geom.js', 'dist/von-canvas2d.js'])
+		.pipe(concat('von-canvas2d-full.js'))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['all']);
